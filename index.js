632492f99213690e628f1b8c079c3e676a38a00b
geogrey12muiruri <greggambugua@gmail.com>
2022-09-04 15:57:17 +0300

    changes

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-function myFunction() {
-    function myFunction() {
-        const x = document.getElementById("forms");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-      }
-}
+const myLogo = document.getElementById('login');
+console.log(myLogo);
