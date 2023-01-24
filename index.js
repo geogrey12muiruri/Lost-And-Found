c64454a9ae61efd90f08bc63829152133be2757a
geogrey12muiruri <greggambugua@gmail.com>
2023-01-25 00:59:17 +0300

    modified the navbar spacing and page layout

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ document.getElementById("signup-link").addEventListener("click", function(e) {
     e.preventDefault();
     $("#signupModal").modal('show');
 });
+$(document).ready(function(){
+    $("#login-link").on("click", function(e) {
+      e.preventDefault();
+      $("#loginModal").modal("show");
+    });
+});
